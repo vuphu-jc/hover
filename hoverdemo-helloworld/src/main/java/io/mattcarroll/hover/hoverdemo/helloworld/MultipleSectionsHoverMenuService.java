20f100061ccd4655d4b53cb7c669e8aecd1cd1cf
vuphu <vuphu.itus@gmail.com>
2020-07-09 10:12:29 +0700

    Fix 7.9.2020

--- hoverdemo-helloworld/src/main/java/io/mattcarroll/hover/hoverdemo/helloworld/MultipleSectionsHoverMenuService.java
+++ hoverdemo-helloworld/src/main/java/io/mattcarroll/hover/hoverdemo/helloworld/MultipleSectionsHoverMenuService.java
@@ -17,6 +17,7 @@ package io.mattcarroll.hover.hoverdemo.helloworld;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -63,7 +64,7 @@ public class MultipleSectionsHoverMenuService extends HoverMenuService {
             mContext = context.getApplicationContext();
 
             mSections = new ArrayList<>();
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < 3; i++) {
                 mSections.add(new Section(
                         new SectionId(String.valueOf(i)),
                         createTabView(),
@@ -71,8 +72,19 @@ public class MultipleSectionsHoverMenuService extends HoverMenuService {
                 ));
             }
 
-
             notifyMenuChanged();
+
+            (new Handler()).postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mSections.add(new Section(
+                            new SectionId(String.valueOf(3)),
+                            createTabView(),
+                            new HoverMenuScreen(mContext, "Screen 1")
+                    ));
+                    notifyMenuChanged();
+                }
+            }, 1000);
         }
 
         int mCounter = 0;
@@ -81,8 +93,9 @@ public class MultipleSectionsHoverMenuService extends HoverMenuService {
             mCounter++;
             ImageView imageView = new ImageView(mContext);
             imageView.setImageResource(R.drawable.tab_background);
-            if (mCounter % 2 == 0)
+            if (mCounter % 2 == 0) {
                 imageView.setImageResource(R.drawable.tab_background_blue);
+            }
             return imageView;
         }
 
